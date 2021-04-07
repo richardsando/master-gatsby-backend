e8abf183d7347cd3efa4679c413d391bc71c8bae
richardsando <richardsando369@gmail.com>
2021-04-07 13:26:33 +1000

    first commit

---
+++ schemas/person.js
@@ -0,0 +1,39 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  name: 'person',
+
+  titles: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      description: 'Tell us a little about yourself',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
---
+++ schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon } from 'react-icons/fa';
+
+export default {
+  // computer name
+  name: 'topping',
+  // visible title
+  titles: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is the topping vegetarian or not',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: ({ name, vegetarian }) => ({
+      title: `${name} ${vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
